6570f6a37bf1cee27d8115c33279fca615f08f72
Abraham <ae248abraham@gmail.com>
2023-10-17 15:39:08 +1100

    Removed watching messages starting with  in MessageCreateListener

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,36 +1,23 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener } from '@sapphire/framework';
 import { Message } from 'discord.js';
-import { DBStandup } from '../lib/database/DBStandup';
 import { DBMessageLogs } from '../lib/database/DBMessageLogs';
 import { PrismaClient } from '@prisma/client';
-import { isTextChannel } from '@sapphire/discord.js-utilities';
 
 const messagelog = async (message: Message, database: PrismaClient) => {
-	// ignore messages sent from bot
-	if (message.author.bot) {
-		return;
-	}
+  // ignore messages sent from bot
+  if (message.author.bot) {
+    return;
+  }
 
-	const logDB = new DBMessageLogs(database);
-	await logDB.messageAdd(Number(message.id), Number(message.author.id), message.author.username, message.content, BigInt(message.channelId));
+  const logDB = new DBMessageLogs(database);
+  await logDB.messageAdd(Number(message.id), Number(message.author.id), message.author.username, message.content, BigInt(message.channelId));
 };
 
 @ApplyOptions<Listener.Options>({ once: false, event: Events.MessageCreate })
 export class MessageCreateListener extends Listener {
-	public override async run(message: Message) {
-		const { database } = this.container;
-		const standupDB = new DBStandup(database);
-		await messagelog(message, database);
-
-		if (message.content.startsWith('$standup') && isTextChannel(message.channel)) {
-			// Get standup content
-			const messages: string = String(message.content);
-			const messageContent: string = messages.slice(8).trim();
-			const teamId = Number(message.channel.parentId);
-			const standupAuthorId = Number(message.author.id);
-
-			await standupDB.standupAdd(teamId, standupAuthorId, Number(message.id), messageContent);
-		}
-	}
+  public override async run(message: Message) {
+    const { database } = this.container;
+    await messagelog(message, database);
+  }
 }
