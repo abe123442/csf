5552a9d234fd2e719ffdb7175e8dda2b2a813912
Abraham <ae248abraham@gmail.com>
2023-10-17 16:01:45 +1100

    Added MessageContent intent to index.ts which fixes empty strings being stored in the message_logs table

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,31 @@ import { GatewayIntentBits, Partials } from 'discord.js';
 import { CseClient } from './cse-client';
 
 const client = new CseClient({
-	logger: {
-		level: LogLevel.Debug
-	},
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.GuildVoiceStates,
-		GatewayIntentBits.GuildPresences
-	],
-	partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.GuildMember, Partials.User]
+  logger: {
+    level: LogLevel.Debug
+  },
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.GuildMember, Partials.User]
 });
 
 const main = async () => {
-	try {
-		client.logger.info('Logging in');
-		await client.login();
-		client.logger.info('Logged in');
-	} catch (error) {
-		client.logger.fatal(error);
-		client.destroy();
-		process.exit(1);
-	}
+  try {
+    client.logger.info('Logging in');
+    await client.login();
+    client.logger.info('Logged in');
+  } catch (error) {
+    client.logger.fatal(error);
+    client.destroy();
+    process.exit(1);
+  }
 };
 
 main();
